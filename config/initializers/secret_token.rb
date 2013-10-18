6cdf5b97c9ace140b84ae8d060a8857f94119aa0
yan_runchen <runchen.yan@drecom.co.jp>
2013-10-18 14:35:54 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoApp::Application.config.secret_key_base = 'a5760763be650232c4a5d03f08989ef35efd9aaffba9a63fdaa04083ab60ca524b994d688b803853af7718c10422a3c4da032abbd929d8d146cf2092130c71ed'
---
+++ vendor/bundle/gems/mysql2-0.3.13/ext/mysql2/siteconf20131018-730-1qwwx0y.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131018-730-v2zj2q"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
